ecad60d186c47e6ad44f224cec643753f8b98f72
sofyura <yurysofein@yandex.ru>
2026-08-28 12:01:29 +0300

    Migrate project to Spring Boot and fix Thymeleaf user form template

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -3,13 +3,12 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import web.model.User;
 import web.service.UserService;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
@@ -19,39 +18,27 @@ public class UserController {
         this.userService = userService;
     }
 
-    // Список всех юзеров
-    @GetMapping("/")
-    public String showAllUsers(Model model) {
+    @GetMapping
+    public String listUsers(@RequestParam(value = "id", required = false) Long id, Model model) {
         model.addAttribute("users", userService.getAllUsers());
-        return "users";
-    }
 
-    // Форма добавления/редактирования (если id не передан, то добавление)
-    @GetMapping("/user-form")
-    public String showUserForm(@RequestParam(value = "id", required = false) Long id, Model model) {
         if (id != null) {
             model.addAttribute("user", userService.getUserById(id));
         } else {
             model.addAttribute("user", new User());
         }
-        return "user-form";
+        return "users";
     }
 
-    // Сохранение или обновление юзера
-    @PostMapping("/save-user")
-    public String saveUser(User user) {
-        if (user.getId() == null) {
-            userService.saveUser(user);
-        } else {
-            userService.updateUser(user);
-        }
-        return "redirect:/";
+    @PostMapping("/save")
+    public String saveUser(@ModelAttribute("user") User user) {
+        userService.saveUser(user);
+        return "redirect:/users";
     }
 
-    // Удаление юзера
-    @PostMapping("/delete-user")
+    @GetMapping("/delete")
     public String deleteUser(@RequestParam("id") Long id) {
         userService.deleteUser(id);
-        return "redirect:/";
+        return "redirect:/users";
     }
 }
